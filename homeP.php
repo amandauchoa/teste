bc8624385d36872c6cadc3296a6932e8bd75c033
amanda uchoa <amandaauchoa_@hotmail.com>
2018-06-15 10:53:30 -0300

    Home Alterada Pela Terceira Vez

--- homeP.php
+++ homeP.php
@@ -1,9 +1,8 @@
 <?php
-
-	include("cabecalho");
+	include("cabecalho2.php");
 	
-	$usuario = "Amanda";
-	$senha = "Amanda";
+	$usuario = "root";
+	$senha = "usbw";
 	$conexao = mysqli_connect ("localhost", $usuario, $senha);
 	
 	if ($conexao){
@@ -12,38 +11,37 @@
 		echo "Não conectou:";
 		echo "Numero:" . mysqli_connect_errno(). "<br />";
 		echo "Descrição:" . mysqli_connect_error(). "<br />";
+		exit;
 	}
 	
-	mysqli_select_db($conexao, "Mercado");
+	mysqli_select_db($conexao, "lista");
 	
-	$sql = "SELECT * FROM produtos";
+	$sql = "SELECT * FROM produto";
 	
 	$resultado = mysqli_query($conexao, $sql);
 	
 	echo "<html>";
 	echo "<table border = '1'>";
 	echo "<tr>";
-	echo "<th>Id do produto:</th>";
-	echo "<th>Nome do produto:</td>";
+	echo "<th>Id:</th>";
+	echo "<th>Nome:</td>";
 	echo "<th>Preço</th>";
 	echo "<th>Quantidade</th>";
-	echo "<th>Unidade</th>";
 	echo "</tr>";
 	
 	while (($vetor = mysqli_fetch_array($resultado)) != NULL){
 		echo "<tr>";
+		echo "<td>" .$vetor["0"]. "</td>";
 		echo "<td>" .$vetor["1"]. "</td>";
 		echo "<td>" .$vetor["2"]. "</td>";
 		echo "<td>" .$vetor["3"]. "</td>";
-		echo "<td>" .$vetor["4"]. "</td>";
-		echo "<td>" .$vetor["5"]. "</td>";
 		echo "</tr>";
 	}
 	
 	echo "</table>";
 	echo "</html>";
 	
-	mysql_close($conexao);
+	mysqli_close($conexao);
 	
 
 ?>
